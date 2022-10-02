57cf4d07e0ddd6635ea41de8a5367585168245e3
folajimiogunbadejo <bekomay26@gmail.com>
2022-10-02 14:48:57 +0100

    add formatter

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,7 +2,7 @@ defmodule CliTest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [ parse_args: 1 ]
+  import Issues.CLI, only: [ parse_args: 1, sort_into_descending_order: 1 ]
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h", "anything"]) == :help
     assert parse_args(["--help", "anything"]) == :help
@@ -14,4 +14,16 @@ defmodule CliTest do
     assert parse_args(["user", "project"]) == { "user", "project", 4 }
   end
 
+  test "sort descending orders the correct way" do
+    result = sort_into_descending_order(fake_created_at_list(
+      ["2018-07-18T11:28:48Z", "2028-07-18T11:28:48Z", "2019-07-18T09:28:48Z", "2019-07-18T09:38:48Z"]
+    ))
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    assert issues == ~w{ 2028-07-18T11:28:48Z 2019-07-18T09:38:48Z 2019-07-18T09:28:48Z 2018-07-18T11:28:48Z }
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values,
+        do: %{"created_at" => value, "other_data" => "xxx"}
+  end
 end
