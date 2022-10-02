6089a2601c27b75fc09484a5e6f495217a05359c
folajimiogunbadejo <bekomay26@gmail.com>
2022-10-02 17:32:05 +0100

    remove duplicate doc

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -14,12 +14,6 @@ defmodule Issues.CLI do
     |> process
   end
 
-  @moduledoc """
-  Handle the command line parsing and the dispatch to
-  the various functions that end up generating a
-  table of the last _n_ issues in a github project
-  """
-
   def run(argv) do
     parse_args(argv)
     |> process()
